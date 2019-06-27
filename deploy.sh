ee68c529dc47efd657cdf1734fc13912c8f36b78
Brendan Saunders <brendan.saunders@nccgroup.com>
2019-06-27 16:10:12 -0400

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ echo "Deployment Target: $DEPLOYMENT_TARGET"
 # 1. KuduSync
 
 if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
-  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE\badazure" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
-  exitWithMessageOnError "Kudu Sync failed"
+  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE/badazure" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
+  #exitWithMessageOnError "Kudu Sync failed"
 fi
 
 
@@ -24,7 +24,7 @@ if [ "$WEBSITE_PYTHON_VERSION" = "3.6" ]; then
 fi
 
 
-if [ -e "$DEPLOYMENT_TARGET/requirements.txt" ]; then
+if [ -f "$DEPLOYMENT_TARGET/requirements.txt" ]; then
   echo "Found requirements.txt"
   echo "Python Virtual Environment: $ANTENV"
   echo "Python Version: $PYTHON3"
@@ -43,4 +43,11 @@ if [ -e "$DEPLOYMENT_TARGET/requirements.txt" ]; then
   pip install -r requirements.txt
 
   echo "pip install finished"
+
+fi
+
+#3. Default Configuration
+
+if [ ! -f "$DEPLOYMENT_TARGET/config.py"]; then
+  echo "Configuration file config.py does not exist, please create."
 fi
